d4f530b1f11026a45db5e7f53ae013c2690d0dfa
achigara <chigara.amin@gmail.com>
2019-04-15 17:02:15 +0100

    add "home" button to pop backstack when fragments are added to backstack

--- app/src/main/java/com/achigara/todo/main/MainActivity.java
+++ app/src/main/java/com/achigara/todo/main/MainActivity.java
@@ -6,8 +6,9 @@ import com.achigara.todo.R;
 import com.achigara.todo.main.view.TodoListFragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentManager.OnBackStackChangedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +19,28 @@ public class MainActivity extends AppCompatActivity {
                     .replace(R.id.container, TodoListFragment.newInstance())
                     .commitNow();
         }
+
+        getSupportFragmentManager().addOnBackStackChangedListener(this);
+        shouldDisplayHomeUp();
+    }
+
+    @Override
+    public void onBackStackChanged() {
+        shouldDisplayHomeUp();
+    }
+
+    public void shouldDisplayHomeUp(){
+        //Enable Up button only  if there are entries in the back stack
+        boolean canGoBack = getSupportFragmentManager().getBackStackEntryCount()>0;
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(canGoBack);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        //This method is called when the up button is pressed. Just the pop back stack.
+        getSupportFragmentManager().popBackStack();
+        return true;
     }
 }
